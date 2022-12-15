d6965f4e836bb94404e7c43a22d70989645e38dc
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2022-12-15 17:46:05 +0100

    minor fix

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,46 +1,47 @@
-<section id="links_section">
+<section id="links_section" class="py-3">
     <div class="container d-flex justify-content-between">
         <div class="links_list">
             <div>
                 <h4>Dc Comics</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Character</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
                 </ul>
                 <h4>SHop</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectables</li>
                 </ul>
             </div>
             <div>
                 <h4>Dc</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Cartificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
                 </ul>
             </div>
             <div>
                 <h4>Sites</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
 
                 </ul>
             </div>
